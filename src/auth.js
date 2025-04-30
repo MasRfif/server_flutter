b49b53fc65341a84eaa6bb4b3d299345704a92ad
MasRfif <odri.naufal@gmail.com>
2025-04-30 07:52:29 +0700

    first commit

---
+++ src/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ error: "Authentication required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
+
+export default authenticate;
